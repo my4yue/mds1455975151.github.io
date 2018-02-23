64808b1e1ced66b8eef4eb1d6ef847d4e4038c7b
mds1455975151 <1455975151@qq.com>
2018-02-23 17:52:20 +0800

    Site updated: 2018-02-23 17:51:43

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 23 2018 16:46:47 GMT+0800 (中国标准时间)
+// build time:Fri Feb 23 2018 17:14:56 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
