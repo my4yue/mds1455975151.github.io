3f897448328d797395fd776083b47097c1d0f4d3
mds1455975151 <1455975151@qq.com>
2018-04-23 18:29:47 +0800

    Site updated: 2018-04-23 18:28:51

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -1,3 +1,3 @@
-// build time:Mon Apr 23 2018 16:13:40 GMT+0800 (中国标准时间)
+// build time:Mon Apr 23 2018 17:16:53 GMT+0800 (中国标准时间)
 $(document).ready(function(){$(document).trigger("bootstrap:before");NexT.utils.isMobile()&&window.FastClick.attach(document.body);NexT.utils.lazyLoadPostsImages();NexT.utils.registerBackToTop();$(".site-nav-toggle button").on("click",function(){var e=$(".site-nav");var o="site-nav-on";var t=e.hasClass(o);var a=t?"slideUp":"slideDown";var i=t?"removeClass":"addClass";e.stop()[a]("fast",function(){e[i](o)})});CONFIG.fancybox&&NexT.utils.wrapImageWithFancyBox();NexT.utils.embeddedVideoTransformer();NexT.utils.addActiveClassToMenuItem();NexT.motion.integrator.add(NexT.motion.middleWares.logo).add(NexT.motion.middleWares.menu).add(NexT.motion.middleWares.postList).add(NexT.motion.middleWares.sidebar);$(document).trigger("motion:before");CONFIG.motion&&NexT.motion.integrator.bootstrap();$(document).trigger("bootstrap:after")});
 //rebuild by neat 
